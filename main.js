2dfa50bbf4a3a98b24115a5457f6390aa96dfef5
Guilherme Moreira dos Santos <66872871+71M-M414@users.noreply.github.com>
2022-01-19 12:20:21 -0300

    Update and rename script.js to main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+
+'use strict'
+
+const openModal = () => document.getElementById('modal')
+    .classList.add('active')
+
+const closeModal = () => {
+    clearFields()
+    document.getElementById('modal').classList.remove('active')
+}
